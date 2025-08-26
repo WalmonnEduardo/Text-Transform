06929113cf652618860961ce1b7de1e16740ba8a
Walmonn Eduardo <130590544+WalmonnEduardo@users.noreply.github.com>
2025-08-26 12:43:43 -0300

    Update Texto.php

--- Texto.php
+++ Texto.php
@@ -1,87 +1,34 @@
-<?php
-class Texto
-{
-    public function alinhar(array $lista)
-    {
-        $listaNova = array();
-        $listaSemAcentos = array();
-        foreach($lista as $l)
-        {
-            $listaSemAcentos[] = $this->removerAcentos($l);
-        }
-        $maior = $this->maiorTexto($listaSemAcentos);
-        $maior += 1;
-        foreach($lista as $l)
-        {
-            $add = $this->contarAcentos($l);
-            $n = $maior+$add;
-            for($i = 0;strlen($l) < $n;$i++)
-            {
-                $l .= " "; 
-            }
-            $listaNova[] = $l;
-        }
-        return $listaNova;
-    }
-    public function maiorTexto(array $lista)
-    {
-        $maior = 0;
-        foreach($lista as $l)
-        {
-            if(strlen($l) > $maior)
-            {
-                $maior = strlen($l);
-            }
-        }
-        return $maior;
-    }
-    public function contarAcentos($texto)
-    {
-        $add = 0;
-        $acentos = array(
-            'Á','À','Â','Ã','Ä','Å',
-            'É','È','Ê','Ë',
-            'Í','Ì','Î','Ï',
-            'Ó','Ò','Ô','Õ','Ö',
-            'Ú','Ù','Û','Ü',
-            'Ç','Ñ',
-            'á','à','â','ã','ä','å',
-            'é','è','ê','ë',
-            'í','ì','î','ï',
-            'ó','ò','ô','õ','ö',
-            'ú','ù','û','ü',
-            'ç','ñ'
-        );
-        for($i = 0;$i < strlen($texto) - 1;$i++)
-        {
-            foreach($acentos as $a)
-            {
-                if(substr($texto,$i,2) == $a)
-                {
-                    $add++;
-                }
-            }
-        }
-        return $add;
 
-    }
-    public function removerAcentos($texto)
-    {
-        $acentos = array(
-            'Á' => 'A', 'À' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A',
-            'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E',
-            'Í' => 'I', 'Ì' => 'I', 'Î' => 'I', 'Ï' => 'I',
-            'Ó' => 'O', 'Ò' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O',
-            'Ú' => 'U', 'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U',
-            'Ç' => 'C', 'Ñ' => 'N',
-            'á' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a',
-            'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
-            'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i',
-            'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o',
-            'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u',
-            'ç' => 'c', 'ñ' => 'n'
-        );
-        return strtr($texto, $acentos);
-    }
+$acentos = array(
+    'Á','À','Â','Ã','Ä','Å',
+    'É','È','Ê','Ë',
+    'Í','Ì','Î','Ï',
+    'Ó','Ò','Ô','Õ','Ö',
+    'Ú','Ù','Û','Ü',
+    'Ç','Ñ',
+    'á','à','â','ã','ä','å',
+    'é','è','ê','ë',
+    'í','ì','î','ï',
+    'ó','ò','ô','õ','ö',
+    'ú','ù','û','ü',
+    'ç','ñ'
+);
+ 
+public function removerAcentos($texto)
+{
+    $acentos = array(
+        'Á' => 'A', 'À' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A',
+        'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+        'Í' => 'I', 'Ì' => 'I', 'Î' => 'I', 'Ï' => 'I',
+        'Ó' => 'O', 'Ò' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O',
+        'Ú' => 'U', 'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U',
+        'Ç' => 'C', 'Ñ' => 'N',
+        'á' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a',
+        'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+        'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i',
+        'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o',
+        'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u',
+        'ç' => 'c', 'ñ' => 'n'
+    );
+    return strtr($texto, $acentos);
 }
-?>
